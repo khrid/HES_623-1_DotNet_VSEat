343149e66fec2b18986ee18ecd73dab601940d02
sylvain.meyer <sylvain.meyer@students.hevs.ch>
2019-10-12 08:22:02 +0200

    Test commit

--- VSEat/ConsoleApp/Program.cs
+++ VSEat/ConsoleApp/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Let's start this project!");
         }
     }
 }
