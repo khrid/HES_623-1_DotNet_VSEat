93542c593a7c8a9a9bbe7c7ce750fdfa9e1129ce
sylvain.meyer <sylvain.meyer@students.hevs.ch>
2019-10-18 14:33:58 +0200

    Ajout des fichiers de création de la base données

---
+++ VSEat/database_creation.sql
@@ -0,0 +1,110 @@
+﻿USE [master]
+GO
+
+/****** Object:  Database [CrittinMeyer_ValaisEat]    Script Date: 18.10.2019 14:08:06 ******/
+DROP DATABASE [CrittinMeyer_ValaisEat]
+GO
+
+/****** Object:  Database [CrittinMeyer_ValaisEat]    Script Date: 18.10.2019 14:08:06 ******/
+CREATE DATABASE [CrittinMeyer_ValaisEat]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'CrittinMeyer_ValaisEat', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL11.MSSQLSERVER\MSSQL\DATA\CrittinMeyer_ValaisEat.mdf' , SIZE = 5120KB , MAXSIZE = UNLIMITED, FILEGROWTH = 1024KB )
+ LOG ON 
+( NAME = N'CrittinMeyer_ValaisEat_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL11.MSSQLSERVER\MSSQL\DATA\CrittinMeyer_ValaisEat_log.ldf' , SIZE = 1024KB , MAXSIZE = 2048GB , FILEGROWTH = 10%)
+GO
+
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [CrittinMeyer_ValaisEat].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+
+ALTER DATABASE [CrittinMeyer_ValaisEat] SET ANSI_NULL_DEFAULT OFF 
+GO
+
+ALTER DATABASE [CrittinMeyer_ValaisEat] SET ANSI_NULLS OFF 
+GO
+
+ALTER DATABASE [CrittinMeyer_ValaisEat] SET ANSI_PADDING OFF 
+GO
+
+ALTER DATABASE [CrittinMeyer_ValaisEat] SET ANSI_WARNINGS OFF 
+GO
+
+ALTER DATABASE [CrittinMeyer_ValaisEat] SET ARITHABORT OFF 
+GO
+
+ALTER DATABASE [CrittinMeyer_ValaisEat] SET AUTO_CLOSE OFF 
+GO
+
+ALTER DATABASE [CrittinMeyer_ValaisEat] SET AUTO_SHRINK OFF 
+GO
+
+ALTER DATABASE [CrittinMeyer_ValaisEat] SET AUTO_UPDATE_STATISTICS ON 
+GO
+
+ALTER DATABASE [CrittinMeyer_ValaisEat] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+
+ALTER DATABASE [CrittinMeyer_ValaisEat] SET CURSOR_DEFAULT  GLOBAL 
+GO
+
+ALTER DATABASE [CrittinMeyer_ValaisEat] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+
+ALTER DATABASE [CrittinMeyer_ValaisEat] SET NUMERIC_ROUNDABORT OFF 
+GO
+
+ALTER DATABASE [CrittinMeyer_ValaisEat] SET QUOTED_IDENTIFIER OFF 
+GO
+
+ALTER DATABASE [CrittinMeyer_ValaisEat] SET RECURSIVE_TRIGGERS OFF 
+GO
+
+ALTER DATABASE [CrittinMeyer_ValaisEat] SET  DISABLE_BROKER 
+GO
+
+ALTER DATABASE [CrittinMeyer_ValaisEat] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+
+ALTER DATABASE [CrittinMeyer_ValaisEat] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+
+ALTER DATABASE [CrittinMeyer_ValaisEat] SET TRUSTWORTHY OFF 
+GO
+
+ALTER DATABASE [CrittinMeyer_ValaisEat] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+
+ALTER DATABASE [CrittinMeyer_ValaisEat] SET PARAMETERIZATION SIMPLE 
+GO
+
+ALTER DATABASE [CrittinMeyer_ValaisEat] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+
+ALTER DATABASE [CrittinMeyer_ValaisEat] SET HONOR_BROKER_PRIORITY OFF 
+GO
+
+ALTER DATABASE [CrittinMeyer_ValaisEat] SET RECOVERY FULL 
+GO
+
+ALTER DATABASE [CrittinMeyer_ValaisEat] SET  MULTI_USER 
+GO
+
+ALTER DATABASE [CrittinMeyer_ValaisEat] SET PAGE_VERIFY CHECKSUM  
+GO
+
+ALTER DATABASE [CrittinMeyer_ValaisEat] SET DB_CHAINING OFF 
+GO
+
+ALTER DATABASE [CrittinMeyer_ValaisEat] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+GO
+
+ALTER DATABASE [CrittinMeyer_ValaisEat] SET TARGET_RECOVERY_TIME = 0 SECONDS 
+GO
+
+ALTER DATABASE [CrittinMeyer_ValaisEat] SET  READ_WRITE 
+GO
+
+
